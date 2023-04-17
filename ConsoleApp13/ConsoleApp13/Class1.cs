b9f9455f00872613114b5c6fc53c8b0e7a55bb67
Feshkineugen <118047190+Feshkineugen@users.noreply.github.com>
2023-04-17 15:46:17 +0300

    Add files via upload

---
+++ ConsoleApp13/ConsoleApp13/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class House
+{
+    public string Address { get; set; }
+    public int Floor { get; set; }
+    public int RoomCount { get; set; }
+    public double Square { get; set; }
+
+    public override string ToString()
+    {
+        return $"Адрес: {Address}, Этаж: {Floor}, Количество комнат: {RoomCount}, Площадь: {Square}";
+    }
+}
